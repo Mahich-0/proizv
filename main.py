2aa3162a224e645dfa9a983662f6ecc511eda955
itcube22 <mishkasosiska331@gmail.com>
2024-11-28 18:51:20 +0700

    get rand generate color

--- main.py
+++ main.py
@@ -31,7 +31,10 @@ class Example(QMainWindow):
 
     def draw_flag(self, qp):
         # Задаем кисть
-        qp.setBrush(QColor(255, 255, 0))
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setBrush(QColor(r, g, b))
         # Рисуем прямоугольник заданной кистью
         d = random.randint(50, 300)
         x = random.randint(0, self.width())
