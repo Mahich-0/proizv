20c64d5081836d0ab07977386eb7cace5232ce0c
itcube22 <mishkasosiska331@gmail.com>
2024-11-28 18:44:38 +0700

    ,j\yhaegf

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+
+    def draw_flag(self, qp):
+        # Задаем кисть
+        qp.setBrush(QColor(255, 255, 0))
+        # Рисуем прямоугольник заданной кистью
+        d = random.randint(50, 300)
+        x = random.randint(0, self.width())
+        y = random.randint(0, self.height())
+        qp.drawEllipse(x, y, d, d)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
